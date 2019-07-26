da93105195fbd625c1ee44b24e08f1f5fbae1a2d
hunanyanyeva <yeva.hunanyan@mail.ru>
2019-07-26 15:35:55 +0400

    session2_tf 99.2

--- nn_homework_session1/neural_network/optimizers.py
+++ nn_homework_session1/neural_network/optimizers.py
@@ -1,12 +1,10 @@
 import numpy as np
-from copy import copy
 
 
 class SGD:
     """
     Simple stochastic gradient descent.
     """
-
     def __init__(self, learning_rate=0.01):
         self.learning_rate = learning_rate
 
@@ -22,22 +20,28 @@ class SGDMomentum:
     def __init__(self, learning_rate=0.01, momentum=0.9):
         self.learning_rate = learning_rate
         self.momentum = momentum
-        self.state = np.array([])
+        self.state = None
 
     def step(self, weights, grad_weights):
-        if self.state == np.array([]):
-            self.state = np.zeros_like(weights)
-        for i, layer_weights, layer_grad_weights in zip(self.state,
-                                                        weights, grad_weights):
-            for j, w, grad_w in zip(self.state, layer_weights,
-                                    layer_grad_weights):
+        if self.state is None:
+            self.state = []
+            for w in weights:
+                self.state.append([])
+                for w_ik in w:
+                    self.state[-1].append(np.zeros_like(w_ik))
+
+        for layer_state, layer_weights, layer_grad_weights in zip(self.state,
+                                                                  weights,
+                                                                  grad_weights):
+            for state, w, grad_w in zip(layer_state, layer_weights,
+                                        layer_grad_weights):
                 if grad_w.shape[0] == 1:
                     w = w.reshape(grad_w)
-                np.add(self.momentum * self.state[i][j],
-                       -self.learning_rate * grad_w, out=self.state[i][j])
-                np.add(w, self.state[i][j], out=w)
+                    state = state.reshape(grad_w)
 
-
+                np.add(-self.learning_rate * grad_w, self.momentum * state,
+                       out=state)
+                np.add(w, state, out=w)
 
 
 class Adam:
